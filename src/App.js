790fe555696cd044aca003f4d21b2fc2e3683967
praphan <praphanhc@gmail.com>
2023-05-07 21:43:27 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+    // console.log("constructor");
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      });
   }
 
   onSearchChange = (event) => {
@@ -23,9 +32,13 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
+    if (this.state.robots.length === 0) {
+      return <h1>Loading!!! Please wait patiently!!!</h1>;
+    }
+    // console.log("render");
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">My Robo Friends App</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
